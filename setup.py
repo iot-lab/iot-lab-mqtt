fea1f56b4ca6b8c74f6d68e17d515d57c8b3077b
Gaëtan Harter <gaetan.harter@inria.fr>
2017-02-21 14:52:57 +0100

    setup.py: add 'iotlab-mqtt-serial' script

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ def get_version(package):
             if line.startswith('__version__'):
                 return eval(line.split('=')[-1])  # pylint:disable=eval-used
 
-SCRIPTS = []
+SCRIPTS = ['iotlab-mqtt-serial']
 
 INSTALL_REQUIRES = ['paho-mqtt>=1.2', 'future']
 
